6ba3aac8eef8ed9d6257312841b9046357083c78
Shubham Aswal <shubhamaswal@Shubhams-MacBook-Air.local>
2025-04-06 20:28:27 +0530

    bmi calculation

--- src/components/common/input/input.tsx
+++ src/components/common/input/input.tsx
@@ -10,6 +10,7 @@ interface InputProps {
   name?: string;
   id?: string;
   error?: string;
+  maxLength?: number;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -22,6 +23,7 @@ const Input: React.FC<InputProps> = ({
   id,
   name,
   error,
+  maxLength,
 }) => {
   const handleWheel = (event: WheelEvent<HTMLInputElement>) => {
     if (type === "number") {
@@ -32,10 +34,22 @@ const Input: React.FC<InputProps> = ({
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (
       type === "number" &&
-      (event.key === "ArrowUp" || event.key === "ArrowDown")
+      (event.key === "ArrowUp" ||
+        event.key === "ArrowDown" ||
+        event.key === "e")
     ) {
-      event.preventDefault(); // Prevent number increment/decrement with arrow keys
+      event.preventDefault(); // Prevent unwanted keys
     }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    if (type === "number" && maxLength !== undefined) {
+      if (inputValue.length > maxLength) return;
+    }
+
+    if (onChange) onChange(event);
   };
 
   return (
@@ -44,13 +58,14 @@ const Input: React.FC<InputProps> = ({
       <input
         type={type}
         placeholder={placeholder}
-        onChange={onChange}
+        onChange={handleChange}
         onBlur={onBlur}
         value={value}
         name={name}
         id={id}
-        onWheel={handleWheel} // Prevents scrolling in number inputs
-        onKeyDown={handleKeyDown} // Prevents up/down key adjustments
+        onWheel={handleWheel}
+        onKeyDown={handleKeyDown}
+        maxLength={type !== "number" ? maxLength : undefined} // only apply if not number
       />
       {error && <span className="error_message">{error}</span>}
     </div>
